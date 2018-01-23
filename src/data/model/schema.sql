589fb32436c428d113213677643bf9a65fd583dd
alexandrawaite <alexandrawaite@gmail.com>
2018-01-23 14:11:14 -0800

    Create pizza db schema

---
+++ src/data/model/schema.sql
@@ -0,0 +1,97 @@
+CREATE TABLE customer (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE address (
+  id SERIAL PRIMARY KEY,
+  address TEXT NOT NULL
+);
+
+CREATE TABLE customer_address (
+  customer_id INTEGER REFERENCES customer,
+  address_id INTEGER REFERENCES address
+);
+
+CREATE TABLE phone_number (
+  id SERIAL PRIMARY KEY,
+  phone_number TEXT
+);
+
+CREATE TABLE customer_phone (
+  customer_id INTEGER REFERENCES customer,
+  phone_id INTEGER REFERENCES phone_number
+);
+
+CREATE TABLE size (
+  id SERIAL PRIMARY KEY,
+  type TEXT,
+  price MONEY
+);
+
+CREATE TABLE crust (
+  id SERIAL PRIMARY KEY,
+  type TEXT
+);
+
+CREATE TABLE ingredient (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  price MONEY
+);
+
+CREATE TABLE pizza (
+  id SERIAL PRIMARY KEY,
+  size_id INTEGER REFERENCES size,
+  crust_id INTEGER REFERENCES crust,
+  price MONEY,
+  is_happy_hour BOOLEAN
+);
+
+CREATE TABLE pizza_ingredient (
+  pizza_id INTEGER REFERENCES pizza,
+  ingredient_id INTEGER REFERENCES ingredient
+);
+
+CREATE TABLE preference (
+  customer_id INTEGER REFERENCES customer,
+  pizza_id INTEGER REFERENCES pizza
+);
+
+CREATE TABLE drink (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  manufacturer TEXT,
+  supplier TEXT,
+  price MONEY
+);
+
+CREATE TABLE credit_card (
+  id SERIAL PRIMARY KEY,
+  card_number INTEGER NOT NULL
+);
+
+CREATE TABLE payment_method (
+  id SERIAL PRIMARY KEY,
+  card_id INTEGER REFERENCES credit_card,
+  type TEXT
+);
+
+CREATE TABLE cart (
+  id SERIAL PRIMARY KEY,
+  customer_id INTEGER REFERENCES customer,
+  payment_method_id INTEGER REFERENCES payment_method
+);
+
+CREATE TABLE pizza_cart (
+  cart_id INTEGER REFERENCES cart,
+  pizza_id INTEGER REFERENCES pizza
+);
+
+CREATE TABLE drink_cart (
+  cart_id INTEGER REFERENCES cart,
+  drink_id INTEGER REFERENCES drink
+);
+
