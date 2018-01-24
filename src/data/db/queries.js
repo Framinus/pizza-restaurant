5206b92e264df343d167f59a9f997c138f405e11
alexandrawaite <alexandrawaite@gmail.com>
2018-01-24 15:42:27 -0800

    write queries for payment and cart

--- src/data/db/queries.js
+++ src/data/db/queries.js
@@ -237,8 +237,107 @@ const deletePreference = (customerId, pizzaId) => {
     WHERE customer_id=$1 AND pizza_id=$2`, [customerId, pizzaId])
 }
 
+// drink queries
+
+const createDrink = (name, manufacturer, supplier, price) => {
+  return db.one(`INSERT INTO drink (name, manufacturer, supplier, price) VALUES ($1, $2, $3, $4)
+  RETURNING *`, [name, manufacturer, supplier, price])
+}
+
+const readAllDrinks = () => {
+  return db.any(`SELECT * FROM drink`)
+}
+
+const readDrinkById = (drinkId) => {
+  return db.one(`SELECT * FROM drink WHERE id=$1`, drinkId)
+}
+
+const updateDrinkById = (drinkId, name, manufacturer, supplier, price) => {
+  return db.one(`UPDATE drink SET name=$1, manufacturer=$2, supplier=$3, price=$4 WHERE id=$5`, [name, manufacturer, supplier, price, drinkId])
+}
+
+const deleteDrinkById = (drinkId) => {
+  return db.oneOrNone(`DELETE FROM drink WHERE id=$1`, drinkId)
+}
+
+// credit card queries
+
+const createCreditCard = (cardNumber) => {
+  return db.one(`INSERT INTO credit_card (card_number) VALUES ($1)
+  RETURNING *`, cardNumber)
+}
+
+const readCreditCardByCustomerId = (customerId) => {
+  return db.one(`SELECT credit_card.card_number
+  FROM credit_card
+  JOIN payment_method ON credit_card.id = payment_method.card_id
+  JOIN cart ON payment_method.id = cart.payment_method_id
+  WHERE cart.customer_id = $1`, customerId)
+}
+
+// const updateCreditCard = () => {
+//
+// }
+
+const deleteCreditCard = (cardId) => {
+  return db.oneOrNone(`DELETE FROM credit_card WHERE id=$1`, cardId)
+}
+
+// payment_method queries
+
+const createPaymentMethod = (cardId, type) => {
+  return db.one(`INSERT INTO payment_method (card_id, type) VALUES ($1, $2)
+  RETURNING *`, [cardId, type])
+}
+
+const readPaymentMethodById = (paymentId) => {
+  return db.one(`SELECT * FROM payment_method WHERE id=$1`, paymentId)
+}
+
+const readAllPaymentMethods = () => {
+  return db.any(`SELECT * FROM payment_method`)
+}
+
+const updatePaymentMethodById = (paymentId, cardId, type) => {
+  return db.one(`UPDATE payment_method SET card_id=$2, type=$3 WHERE id=$1
+    RETURNING *`, [paymentId, cardId, type])
+}
+
+const deletePaymentMethodById = (paymentId) => {
+  return db.oneOrNone(`DELETE FROM payment_method WHERE id=$1`, paymentId)
+}
+
+// cart queries
+
+const createCart = (customerId, paymentId, isHappyHour, isDelivery) => {
+  return db.one(`INSERT INTO cart (customer_id, payment_method_id, is_happy_hour, is_delivery) VALUES ($1, $2, $3, $4)
+  RETURNING *`, [customerId, paymentId, isHappyHour, isDelivery])
+}
+
+const createPizzaCart = (cartId, pizzaId) => {
+  return db.one(`INSERT INTO pizza_cart (cart_id, pizza_id) VALUES ($1, $2)
+  RETURNING *`, [cartId, pizzaId])
+}
+
+const createDrinkCart = (cartId, drinkId) => {
+  return db.one(`INSERT INTO drink_cart (cart_id, drink_id) VALUES ($1, $2)
+  RETURNING *`, [cartId, drinkId])
+}
+
+const readCartById = (cartId) => {
+  return db.one(`SELECT * FROM cart WHERE id=$1`, cartId)
+}
+
+const updateCartById = (cartId, paymentId, isDelivery) => {
+  return db.one(`UPDATE cart SET payment_method_id=$2, is_delivery=$3 WHERE id=$1
+  RETURNING *`, [cartId, paymentId, isDelivery])
+}
+
+const deleteCartById = (cartId) => {
+  return db.oneOrNone(`DELETE FROM cart WHERE id=$1`, cartId)
+}
 
 // Modify getSizePrice
 // Call above two functions, add results in JavaScript and insert results into createPizza
 
-module.exports = { createCustomer, readCustomerById, editCustomerById, deleteCustomerById, createAddress, readAddressById, editAddressById, deleteAddressById, createPhone, readPhoneById, editPhoneById, deletePhoneById, createSize, readAllSizes, readSizeById, updateSizeById, deleteSizeById, createCrust, readAllCrusts, readCrustById, updateCrustById, deleteCrustById, createIngredient, readAllIngredients, readIngredientById, updateIngredientById, deleteIngredientById, getPizzaIngredientPrice, getSizePrice, createPizza, readPizzaById, updatePizzaById, deletePizzaById, createPizzaIngredients, readPizzaIngredientsById, deletePizzaIngredientById, createPreference, readTypeAndCrustPreferences, readIngredientPreferences, deletePreference };
+module.exports = { createCustomer, readCustomerById, editCustomerById, deleteCustomerById, createAddress, readAddressById, editAddressById, deleteAddressById, createPhone, readPhoneById, editPhoneById, deletePhoneById, createSize, readAllSizes, readSizeById, updateSizeById, deleteSizeById, createCrust, readAllCrusts, readCrustById, updateCrustById, deleteCrustById, createIngredient, readAllIngredients, readIngredientById, updateIngredientById, deleteIngredientById, getPizzaIngredientPrice, getSizePrice, createPizza, readPizzaById, updatePizzaById, deletePizzaById, createPizzaIngredients, readPizzaIngredientsById, deletePizzaIngredientById, createPreference, readTypeAndCrustPreferences, readIngredientPreferences, deletePreference, createDrink, readAllDrinks, readDrinkById, updateDrinkById, deleteDrinkById, createCreditCard, readCreditCardByCustomerId, deleteCreditCard, createPaymentMethod, readPaymentMethodById, readAllPaymentMethods, updatePaymentMethodById, deletePaymentMethodById, createCart, createPizzaCart, createDrinkCart, readCartById, updateCartById, deleteCartById };
